fa380c12f1b8c461292b299cb55aa7124a1a2f9e
Антон <ahramcov2018@yandex.ru>
2023-03-13 21:02:17 +0300

    Первая версия, перевод изображения в градацию серого и создание массивов пикселей

---
+++ main.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import numpy as np
+
+filename = 'D:\\Программы\\ML\\image\\1.jpg'
+with Image.open(filename) as img:
+    img.load()
+
+matrix_img = np.array(img.getdata()).reshape(img.size[0], img.size[1], 3) # массив значений rgb пикселей, в формате строка/столбец/цветовой канал
+
+gray_img = img.convert("L") #Перевод изображения в градации серого
+
+matrix_gray_img = np.array(gray_img.getdata()).reshape(gray_img.size[0], gray_img.size[1]) # массив значений градаций серого для каждого пикселя, в формате строка/столбец
