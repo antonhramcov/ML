ec89f81d2cadfd0ed3211141b1faf52e804e0c4a
Антон <ahramcov2018@yandex.ru>
2023-03-14 20:15:58 +0300

    Влад, чекай, закомментил все)))

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ matrix_img = np.array(img.getdata()).reshape(img.size[0], img.size[1], 3) # ма
 gray_img = img.convert("L") #Перевод изображения в градации серого
 
 matrix_gray_img = np.array(gray_img.getdata()).reshape(gray_img.size[0], gray_img.size[1]) # массив значений градаций серого для каждого пикселя, в формате строка/столбец
+
+new_matrix_gray_img = np.zeros((gray_img.size[0]*2,gray_img.size[1]), dtype=np.uint8)
+j=0
+for i in range(0,len(new_matrix_gray_img),2):
+    new_matrix_gray_img[i] = matrix_img[j]
+    j += 1
+
+
+
+
+#new_img = Image.fromarray(new_gray_img, mode='RGB')
